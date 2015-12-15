b8f6873c5d921ab6a31219042de3d0a9d9f8f1df
Benoit Beausejour <b@turbulent.ca>
2015-12-15 14:37:58 -0500

    Add dependencies to PyYAML and setuptools.

--- setup.py
+++ setup.py
@@ -3,14 +3,18 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
+version = '0.1'
+install_requires = ['setuptools', 'PyYAML']
+
 setup(name='dockwrkr',
-      version='0.1',
+      version=version,
       author='Turbulent/bbeausej',
       author_email='b@turbulent.ca',
       license='MIT',
       long_description=readme,
-      description='dockwrkr - Launch and manage docker containers',
-      packages=find_packages(),
+      description='dockwrkr - docker container launch wrapper',
+      install_requires=install_requires,
+      packages=['dockwrkr'],
       entry_points={
         'console_scripts': [
           'dockwrkr = dockwrkr:main',
